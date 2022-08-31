2f5fc62bc543e1ae8eb729dacc3085c444e3732a
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-08-31 13:52:57 -0400

    Java Files Updated

--- java/HappyNumbers.java
+++ java/HappyNumbers.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashSet; 
 import java.util.List;
+import java.lang.Math;
 //Test Comment
 public class HappyNumbers {
 
@@ -13,20 +14,32 @@ public class HappyNumbers {
        if (num1 > num2){
            num1 = num1 ^ num2 ^ (num2 = num1);
        }//if the first number is larger than the second number, swap the numbers
-       ArrayList<int> happyNums = new ArrayList<int>();
+       ArrayList<Integer> happyNums = new ArrayList<Integer>();
        
        System.out.println("First Argument: "+args[0]+"\nSecond Argument: "+args[1]);
        for (int current = num1; current <= num2; current++)
            if (isHappy(current)) {
-               System.out.println(current);
+               //System.out.println(current);
                happyNums.add(current);
                //total++;
            }
+        
+        if (happyNums.size() == 0){
+            System.out.println("NOBODYS HAPPY!");
+        }
+        else{
+             Map<Double, Integer> map = new TreeMap<Double, Integer>(Collections.reverseOrder());
+            for (int i = 0; i < happyNums.size(); i++){
+                normCheck(happyNums.get(i), map);
+            }
+            printMap(map);
+        }
+       
    }
 
    public static boolean isHappy(int number) {
        HashSet<Integer> cycle = new HashSet<>();
-       List<String> normSum
+       //List<String> normSum
        while (number != 1 && cycle.add(number)) {
            List<String> numStrList = Arrays.asList(String.valueOf(number).split(""));
            number = numStrList.stream().map(i -> Math.pow(Integer.parseInt(i), 2)).mapToInt(i -> i.intValue()).sum();
@@ -35,14 +48,29 @@ public class HappyNumbers {
        return number == 1;
    }//method found on rosetta code to check to see if the numbers are happy
 
-   public static boolean normCheck(int number) {
-       HashSet<Integer> cycle = new HashSet<>();
-       List<String> normSum
-       while (number != 1 && cycle.add(number)) {
-           List<String> numStrList = Arrays.asList(String.valueOf(number).split(""));
-           number = numStrList.stream().map(i -> Math.pow(Integer.parseInt(i), 2)).mapToInt(i -> i.intValue()).sum();
-
+   public static void normCheck(int number, Map<Double, Integer> map) {
+       //HashSet<Integer> cycle = new HashSet<>();
+       //List<String> normSum
+       int tempNum = number; 
+       double normSum = Math.pow(number,2);
+       
+       while (number != 1) {
+            List<String> numStrList = Arrays.asList(String.valueOf(number).split(""));
+            number = numStrList.stream().map(i -> Math.pow(Integer.parseInt(i), 2)).mapToInt(i -> i.intValue()).sum();
+            normSum += Math.pow(number, 2);
        }
-       return number == 1;
+       double norm = Math.sqrt(normSum+1);
+       map.put(norm,tempNum);
    }//method found on rosetta code to check to see if the numbers are happy
+
+   public static void printMap(Map<Double, Integer> map){
+    
+        TreeMap<Double, Integer> newMap = map.entrySet().stream()
+            .limit(10)
+            .collect(TreeMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+
+        for (Double key: newMap.keySet()) {
+            System.out.println(newMap.get(key));
+        }
+    }
 }
