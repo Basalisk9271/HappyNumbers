50b331cf3d2fdc06cd8fe355c3e9e56cb294a1e5
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-09-06 19:13:53 -0400

    C# File Updates

---
+++ c#/HappyNumbers.cs
@@ -0,0 +1,95 @@
+// Include namespace system
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Collections;
+
+
+
+public class HappyNumbers
+{
+    public static void Main()
+    {
+        Console.Write("First Argument: ");
+        var num1 = int.Parse(Console.ReadLine());
+        Console.Write("Second Argument: ");
+        var num2 = int.Parse(Console.ReadLine());
+        // take in arguments from command line
+        if (num1 > num2)
+        {
+            num1 = num1 ^ num2 ^ (num2 = num1);
+        }
+        // if the first number is larger than the second number, swap the numbers
+        int happyNumCount = 0;
+        var normDict = new SortedDictionary<double, int>();
+        for (int current = num1; current <= num2; current++)
+        {
+            if (HappyNumbers.isHappy(current, ref normDict))
+            {
+                happyNumCount++;
+            }
+        }// for loop to check if there any happy numbers in the range specified by the arguments
+        if (happyNumCount == 0)
+        {
+            Console.WriteLine("NOBODYS HAPPY!");
+        }
+        else 
+        {
+            HappyNumbers.printDict(normDict);
+        }
+    }// main method
+    
+    public static bool isHappy(int number, ref SortedDictionary<double,int> dict)
+    {
+        List<int> cache = new List<int>();
+        int sum = 0;
+        int initialNumber = number; //holds the initial number
+        double runningTotalForNorm = Math.Pow(initialNumber,2)+1; //Started norm off with 1 since I need to add 1 at the end
+        while (number != 1)
+        {
+            if (cache.Contains(number))
+            {
+                return false;
+            }
+            cache.Add(number);
+            while (number != 0)
+            {
+                int digit = number % 10;
+                
+                sum += digit * digit;
+                
+                number /= 10;
+
+            }
+            number += sum;
+            runningTotalForNorm += Math.Pow(number,2);
+            sum = 0;
+        }
+        double norm = Math.Sqrt(runningTotalForNorm);
+        dict.Add(norm, initialNumber);
+        return true;        
+    }// method found on rosetta code to check to see if the numbers are happy
+    
+    public static void printDict(SortedDictionary <double, int> dict)
+    {
+        var items = from pair in dict
+            orderby pair.Key descending
+            select pair;
+
+        var counter = 0;
+        foreach (KeyValuePair<double, int> pair in items)
+        {
+         
+        
+            if (counter >= 10){
+                break;
+            }
+            else{
+                Console.WriteLine(pair.Value);
+                counter++;  
+            }   
+        }
+    }//
+}
