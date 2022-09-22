8220b6d89253d687ef54b5f60ffe04e7bdab06ca
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-09-22 11:06:00 -0400

    C File Updates

--- c/HappyNumbers.c
+++ c/HappyNumbers.c
@@ -1,1 +1,17 @@
 //This is my c program for Happy Num
+#include <stdio.h>
+
+void main(){
+    int lowerBound, upperBound;
+    printf("First Argument: ");
+    scanf ("%d", &lowerBound);
+    printf("Second Argument: ");
+    scanf ("%d", &upperBound);
+
+    if (lowerBound > upperBound) {
+        int temp = upperBound;
+        upperBound = lowerBound;
+        lowerBound = temp;
+    }
+    
+}
