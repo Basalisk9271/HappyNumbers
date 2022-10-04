4feef2f4eee837581861733ddf911d73921fdde0
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-10-04 00:48:59 -0400

    Fortran File Updates

--- fortran/HappyNumbers.f95
+++ fortran/HappyNumbers.f95
@@ -1,13 +1,43 @@
+!This is my Fortran code for my HappyNumbers Project
+!Tutorials Point is where I found most of my structure and how to call different things
+!https://www.tutorialspoint.com/fortran/index.htm 
+
 PROGRAM HappyNumbers
     IMPLICIT NONE
+    
+    type KVPair
+        integer :: happyNum
+        real :: norm
+    end type
 
-    integer :: lowerBound, upperBound
+    integer :: lowerBound, upperBound, temp, i
+    !real :: norm
+    type(KVPair), dimension(10) :: happyArray
 
     write(*,"(A)",advance="no") "First Argument: "
     read *,lowerBound
     write(*,"(A)",advance="no") "Second Argument: "
     read *,upperBound
-    call printHappy(lowerBound, upperBound)
+    !call printHappy(lowerBound, upperBound)
+
+    if ( lowerBound > upperBound ) then
+        temp = upperBound
+        upperBound = lowerBound
+        lowerBound = temp
+    end if
+
+    do i = 1, 10
+        happyArray(i)%happyNum = 0
+        happyArray(i)%norm = 1.0
+    end do
+
+    do i = 1, 10
+        WRITE(*, '(i0)') happyArray(i)%happyNum
+        WRITE(*, '(F0.6)') happyArray(i)%norm
+    end do
+
+
+
 
 END PROGRAM HappyNumbers
 
@@ -19,20 +49,20 @@ END PROGRAM HappyNumbers
         do i = 1, number+1
             
         end do
-        
+        isHappy = numDivisors
 
     end function isHappy
     
-    subroutine printHappy(lowerBound, upperBound) 
-        IMPLICIT NONE
-        integer, intent(in) :: lowerBound, upperBound
-        integer :: i
-        logical :: isPrime
-        do i = lowerBound, upperBound
-            if (isPrime(i)) then
-                WRITE(*, '(i0)') i
-            end if            
-        end do
+    !subroutine printHappy(happyArray) 
+     !   IMPLICIT NONE
+      !  type(KVPair), intent(in) :: happyArray(10)
+       ! integer :: i
+        !logical :: isPrime
+        !do i = lowerBound, upperBound
+         !   if (isPrime(i)) then
+          !      WRITE(*, '(i0)') i
+           ! end if            
+        !end do
         
 
-    end subroutine printHappy
+    !end subroutine printHappy
