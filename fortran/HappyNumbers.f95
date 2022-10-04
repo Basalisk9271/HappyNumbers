5e608b153b85cd983098387fd8f93f699975b225
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-10-04 11:42:20 -0400

    Fortran File Updates

--- fortran/HappyNumbers.f95
+++ fortran/HappyNumbers.f95
@@ -10,8 +10,13 @@ PROGRAM HappyNumbers
         real :: norm
     end type
 
+    type Node
+        integer :: key
+        type(Node), pointer :: next => null ()
+    end type
+
     integer :: lowerBound, upperBound, temp, i
-    !real :: norm
+    real :: norm
     type(KVPair), dimension(10) :: happyArray
 
     write(*,"(A)",advance="no") "First Argument: "
@@ -36,33 +41,65 @@ PROGRAM HappyNumbers
         WRITE(*, '(F0.6)') happyArray(i)%norm
     end do
 
+    do i = lowerBound, upperBound
 
+        if (isHappy(i) /= -1) then
+            norm = isHappy(i)
+            if ( norm > happyArray(9)%norm ) then
+                happyArray(9)%happyNum = i
+                happyArray(9)%norm = norm
+            end if
+            call Bubble_Sort(happyArray)
+        end if
+    end do
 
+    do i = 1, 10
+        WRITE(*, '(i0)') happyArray(i)%happyNum
+        WRITE(*, '(F0.6)') happyArray(i)%norm
+    end do
 
-END PROGRAM HappyNumbers
-
-    integer function isHappy(number)
+    contains
+    real function isHappy(number)
         IMPLICIT NONE
+        integer(kind = 8), dimension (:), allocatable :: cache
         integer, intent(in):: number
-        integer :: numDivisors, i
-        numDivisors = 0
-        do i = 1, number+1
-            
-        end do
-        isHappy = numDivisors
+        integer(kind = 8) :: temp
+        integer :: i, sum
+        allocate(cache(number))
 
+        do while () 
+
+        end do
     end function isHappy
     
-    !subroutine printHappy(happyArray) 
-     !   IMPLICIT NONE
-      !  type(KVPair), intent(in) :: happyArray(10)
-       ! integer :: i
-        !logical :: isPrime
-        !do i = lowerBound, upperBound
-         !   if (isPrime(i)) then
-          !      WRITE(*, '(i0)') i
-           ! end if            
-        !end do
+    logical function searchLL(head, x) 
+       IMPLICIT NONE
+       type(KVPair), intent(in) :: happyArray(10)
+       integer, intent(in) :: 
+       integer :: i
+       logical :: isPrime
         
 
-    !end subroutine printHappy
+    end function
+
+    !This bubble sort is one found on Rosetta Code and tweaked to bubble sort my array of structs
+    SUBROUTINE Bubble_Sort(a)
+        type(KVPair), INTENT(in out), DIMENSION(:) :: a
+        type(KVPair) :: temp
+        INTEGER :: i, j
+        LOGICAL :: swapped
+       
+        DO j = SIZE(a)-1, 1, -1
+          swapped = .FALSE.
+          DO i = 1, j
+            IF (a(i)%norm > a(i+1)%norm) THEN
+              temp = a(i)
+              a(i) = a(i+1)
+              a(i+1) = temp
+              swapped = .TRUE.
+            END IF
+          END DO
+          IF (.NOT. swapped) EXIT
+        END DO
+      END SUBROUTINE Bubble_Sort
+END PROGRAM HappyNumbers
