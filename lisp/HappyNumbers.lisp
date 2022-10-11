a9db826daf8233092bf6f23a9c9f3d73f9508c1c
Gabriel A Imlay <gabe.imlay9271@gmail.com>
2022-10-11 00:36:11 -0400

    Lisp File Updates

--- lisp/HappyNumbers.lisp
+++ lisp/HappyNumbers.lisp
@@ -5,25 +5,21 @@
 ;Project 1: Happy Numbers -> Lisp
 ;October 7th, 2022
 
-(defun printHappyNums (num1 num2)
-  (loop for i from num1 to num2 by 1 do
-        ( when (isprime i) (print i) )
-        )
-  (terpri)
-  )
+(defstruct (KVPair(:constructor create-kvpair))
+  happynum
+  norm
+)
 
 ;;; Here is the function to determine if a number is prime
-(defun ishappy ( num )
-  (let (( numDivisors 0))
-    (loop for i from 1 to num by 1 do
-          ( when ( = (mod num i) 0 )
-                 ( setf  numDivisors (+ numDivisors 1))
-                 )
-          )
-    ;;; This last function should return true for primes
-    (= numDivisors 2)
-    )
+(defun ishappy ( param )
+  defvar number temp digit sum runningTotalForNorm
+  defvar norm
+  (setf number param)
+  (setf runningTotalForNorms 1)
+  (loop while (!= number 1)
+    do()  
   )
+)
 
 
 ;;; Here is the main program
@@ -31,11 +27,22 @@
   ;;; Let's define variables to avoid compiler warnings
   (defvar num1)
   (defvar num2)
+  (defvar newpair)
+  (defvar happyarray)
   (princ "Enter the first number:")
   (terpri)
   (setf num1 (read))
   (princ "Enter the second number:")
   (terpri)
   (setf num2 (read))
-  (printprimes num1 num2)
+  (if (> num1 num2)
+    (rotatef num1 num2)
+  ) 
+  (setq newpair (create-kvpair :happynum 0 :norm 0))
+  ;(write newPair)
+  (terpri)
+  (setf happyarray (make-array '(10)))
+  (loop for x from 0 to 9
+    do(setf (aref happyarray x) (create-kvpair :happynum 0 :norm 0))
+  )
   )
